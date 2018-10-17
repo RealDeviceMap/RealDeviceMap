b771c00bc634aef934767df0b622c482d9996cd1
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-10-17 19:59:15 +0200

    fix migration

--- resources/migrations/11.sql
+++ resources/migrations/11.sql
@@ -1,1 +1,4 @@
-ALTER TABLE gym MODIFY `raid_pokemon_form` tinyint(6) unsigned DEFAULT NULL;
+ALTER TABLE gym 
+MODIFY `raid_pokemon_form` tinyint(3) unsigned DEFAULT NULL;
+ALTER TABLE gym 
+ADD COLUMN `raid_is_exclusive` tinyint(1) unsigned DEFAULT NULL;
