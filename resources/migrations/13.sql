2992ed6a171bc58b3843f8a8f7022e25c5542e47
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-10-29 19:35:26 +0100

    fix migration

--- resources/migrations/13.sql
+++ resources/migrations/13.sql
@@ -5,7 +5,7 @@ MODIFY `data` longtext NOT NULL;
 ALTER TABLE account
 ADD COLUMN `level` tinyint(3) unsigned NOT NULL DEFAULT 0;
 UPDATE account
-SET level = 30 
+SET level = 30
 WHERE is_high_level = true;
 ALTER TABLE account
 DROP COLUMN `is_high_level`;
@@ -26,6 +26,8 @@ ADD COLUMN `quest_conditions` text DEFAULT NULL;
 ALTER TABLE pokestop
 ADD COLUMN `quest_rewards` text DEFAULT NULL;
 ALTER TABLE pokestop
+ADD COLUMN `quest_template` varchar(100) DEFAULT NULL;
+ALTER TABLE pokestop
 ADD COLUMN `quest_pokemon_id` smallint(6) unsigned  GENERATED ALWAYS AS (JSON_EXTRACT(JSON_EXTRACT(quest_rewards, '$[*].info.pokemon_id'), '$[0]') );
 ALTER TABLE pokestop
 ADD INDEX `ix_quest_quest_pokemon_id` (quest_pokemon_id);
@@ -33,5 +35,14 @@ ALTER TABLE pokestop
 ADD COLUMN `quest_reward_type` smallint(6) unsigned GENERATED ALWAYS AS (JSON_EXTRACT(JSON_EXTRACT(quest_rewards, '$[*].type'), '$[0]') );
 ALTER TABLE pokestop
 ADD INDEX `ix_quest_reward_type` (quest_reward_type);
-ALTER TABLE pokestop
-ADD INDEX `ix_quest_type` (quest_type);
+ALTER TABLE `group`
+ADD COLUMN `perm_view_map_quest` tinyint(1) unsigned NOT NULL;
+UPDATE `group`
+SET perm_view_map_quest = 1
+WHERE name = "root";
+UPDATE `group`
+SET perm_view_map_quest = 1
+WHERE name = "default";
+UPDATE `group`
+SET perm_view_map_quest = 1
+WHERE name = "no_user";
--- resources/migrations/14.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE pokestop
-ADD COLUMN `quest_template` varchar(100)  DEFAULT NULL;
