161bb0250b2676455f2e9c1cd67fcb5293a351de
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-11-03 01:31:43 +0100

    i am tired

--- resources/migrations/17.sql
+++ resources/migrations/17.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `quest_stats` (
   `date` DATE NOT NULL,
   `reward_type` smallint(6) unsigned NOT NULL DEFAULT 0,
-  `pokemon_id` smallint(6) unsigned DEFAULT NULL DEFAULT 0,
-  `item_id` smallint(6) unsigned DEFAULT NULL DEFAULT 0,
+  `pokemon_id` smallint(6) unsigned NOT NULL DEFAULT 0,
+  `item_id` smallint(6) unsigned NOT NULL DEFAULT 0,
   `count` int NOT NULL,
   PRIMARY KEY (`date`, `reward_type`, `pokemon_id`, `item_id`)
 );
@@ -16,7 +16,7 @@ FOR EACH ROW BEGIN
   IF ((OLD.quest_type IS NULL OR OLD.quest_type = 0) AND (NEW.quest_type IS NOT NULL AND NEW.quest_type != 0)) THEN
     INSERT INTO quest_stats (reward_type, pokemon_id, item_id, count, date)
     VALUES
-      (NEW.quest_reward_type, NEW.quest_pokemon_id, NEW.quest_item_id, 1, DATE(FROM_UNIXTIME(NEW.quest_timestamp)))
+      (NEW.quest_reward_type, IFNULL(NEW.quest_pokemon_id, 0), IFNULL(NEW.quest_item_id, 0), 1, DATE(FROM_UNIXTIME(NEW.quest_timestamp)))
     ON DUPLICATE KEY UPDATE
       count = count + 1&semi
   END IF&semi
@@ -28,7 +28,7 @@ FOR EACH ROW BEGIN
   IF (NEW.quest_type IS NOT NULL AND NEW.quest_type != 0) THEN
     INSERT INTO quest_stats (reward_type, pokemon_id, item_id, count, date)
     VALUES
-      (NEW.quest_reward_type, NEW.quest_pokemon_id, NEW.quest_item_id, 1, DATE(FROM_UNIXTIME(NEW.quest_timestamp)))
+      (NEW.quest_reward_type, IFNULL(NEW.quest_pokemon_id, 0), IFNULL(NEW.quest_item_id, 0), 1, DATE(FROM_UNIXTIME(NEW.quest_timestamp)))
     ON DUPLICATE KEY UPDATE
       count = count + 1&semi
   END IF&semi
