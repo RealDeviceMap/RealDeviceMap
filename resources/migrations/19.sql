62d475f96acded50929bfe2d89f87765b123e9be
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-11-07 23:26:19 +0100

    fix for mysql

--- resources/migrations/19.sql
+++ resources/migrations/19.sql
@@ -1,2 +1,4 @@
 ALTER TABLE pokemon
-ADD COLUMN `changed` int(11) NOT NULL DEFAULT `first_seen_timestamp`;
+ADD COLUMN `changed` int(11) unsigned NOT NULL DEFAULT 0;
+UPDATE pokemon
+SET changed = first_seen_timestamp;
