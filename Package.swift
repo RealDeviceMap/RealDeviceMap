a9a7ea76c0a100f469cfd3750b9779c0bb2bbcf3
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-04-17 10:05:38 +0200

    Fix Turnstile Version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", .upToNextMinor(from: "3.0.2")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", .upToNextMinor(from: "3.1.0")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", .upToNextMinor(from: "3.3.0")),
-        .package(url: "https://github.com/123FLO321/Turnstile.git", .upToNextMinor(from: "1.2.1")),
+        .package(url: "https://github.com/123FLO321/Turnstile.git", .upToNextMinor(from: "1.2.2")),
         .package(url: "https://github.com/crossroadlabs/Regex.git", .upToNextMinor(from: "1.1.0")),
         .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.5.0")),
         .package(url: "https://github.com/123FLO321/turf-swift.git", .upToNextMinor(from: "0.3.1")),
