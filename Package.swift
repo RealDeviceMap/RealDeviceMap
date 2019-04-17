ad9a807d450397cb5d168f2df0b1bb3bdbe46f41
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-04-17 08:46:59 +0200

    Update Dependencies, Fix Package.swift for Swift 5 toolchain

--- Package.swift
+++ Package.swift
@@ -6,21 +6,22 @@ let package = Package(
     name: "RealDeviceMap",
     products: [],
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.18"),
-        .package(url: "https://github.com/123FLO321/Perfect-Session-MySQL.git", from: "3.1.6"),
-        .package(url: "https://github.com/123FLO321/Perfect-MySQL.git", from: "3.2.2"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.5"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.2"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.7"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", from: "3.3.0"),
-        .package(url: "https://github.com/stormpath/Turnstile.git", from: "1.0.6"),
-        .package(url: "https://github.com/crossroadlabs/Regex.git", from: "1.1.0"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.2.0"),
-        .package(url: "https://github.com/123FLO321/turf-swift.git", from: "0.3.1"),
-        .package(url: "https://github.com/123FLO321/S2Geometry.git", from: "0.3.1"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.0.22")),
+        .package(url: "https://github.com/123FLO321/Perfect-Session-MySQL.git", .upToNextMinor(from: "3.2.3")),
+        .package(url: "https://github.com/123FLO321/Perfect-MySQL.git", .upToNextMinor(from: "3.2.2")),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", .upToNextMinor(from: "3.0.6")),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", .upToNextMinor(from: "3.0.2")),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", .upToNextMinor(from: "3.1.0")),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", .upToNextMinor(from: "3.3.0")),
+        .package(url: "https://github.com/123FLO321/Turnstile.git", .upToNextMinor(from: "1.2.1")),
+        .package(url: "https://github.com/crossroadlabs/Regex.git", .upToNextMinor(from: "1.1.0")),
+        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.5.0")),
+        .package(url: "https://github.com/123FLO321/turf-swift.git", .upToNextMinor(from: "0.3.1")),
+        .package(url: "https://github.com/123FLO321/S2Geometry.git", .upToNextMinor(from: "0.3.1")),
         .package(url: "https://github.com/123FLO321/POGOProtos-Swift.git", .upToNextMinor(from: "1.6.0")),
     ],
     targets: [
-        .target(name: "RealDeviceMap", dependencies: ["PerfectHTTPServer","PerfectSessionMySQL","PerfectMySQL","PerfectThread","PerfectMustache","PerfectCURL","Turnstile","Regex","POGOProtos","Turf","S2Geometry", "PerfectSMTP"])
-    ]
+        .target(name: "RealDeviceMap", dependencies: ["PerfectHTTPServer","PerfectSessionMySQL","PerfectMySQL","PerfectThread","PerfectMustache","PerfectCURL","PerfectSMTP","Turnstile","Regex","POGOProtos","Turf","S2Geometry"])
+    ],
+    swiftLanguageVersions: [4]
 )
