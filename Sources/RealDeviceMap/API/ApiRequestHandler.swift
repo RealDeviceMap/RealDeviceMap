33d7748911c3976f6901a9c56d34d98302beb1f7
versx <versefx@gmail.com>
2019-05-16 10:57:24 -0700

    Group assignment buttons, change edit button to primary color

--- Sources/RealDeviceMap/API/ApiRequestHandler.swift
+++ Sources/RealDeviceMap/API/ApiRequestHandler.swift
@@ -1045,7 +1045,7 @@ class ApiRequestHandler {
                         assignmentData["time"] = ["timestamp": assignment.time as Any, "formatted": formattedTime]
                        
                         let instanceUUID = "\(assignment.instanceName.escaped())\\-\(assignment.deviceUUID.escaped())\\-\(assignment.time)"
-                        assignmentData["buttons"] = "<a href=\"/dashboard/assignment/edit/\(instanceUUID.encodeUrl()!)\" role=\"button\" class=\"btn btn-info\">Edit</a><a href=\"/dashboard/assignment/delete/\(instanceUUID.encodeUrl()!)\" role=\"button\" class=\"btn btn-danger\">Delete</a>"
+                        assignmentData["buttons"] = "<div class=\"btn-group\" role=\"group\"><a href=\"/dashboard/assignment/edit/\(instanceUUID.encodeUrl()!)\" role=\"button\" class=\"btn btn-primary\">Edit</a><a href=\"/dashboard/assignment/delete/\(instanceUUID.encodeUrl()!)\" role=\"button\" class=\"btn btn-danger\">Delete</a></div>"
                     } else {
                         assignmentData["time"] = assignment.time as Any
                     }
