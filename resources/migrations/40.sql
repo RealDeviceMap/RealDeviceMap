9d3448caa1f017d89caa2b7c425392d6a91ed7e9
versx <versefx@gmail.com>
2019-07-23 01:57:33 -0700

    Add incident_expire_timestamp index

--- resources/migrations/40.sql
+++ resources/migrations/40.sql
@@ -3,3 +3,6 @@ ADD COLUMN `pokestop_display` smallint(5) DEFAULT 0;
 
 ALTER TABLE `pokestop`
 ADD COLUMN `incident_expire_timestamp` int(11) unsigned DEFAULT NULL;
+
+ALTER TABLE `pokestop`
+ADD KEY `ix_incident_expire_timestamp` (`incident_expire_timestamp`);
