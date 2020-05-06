a8c9f8518474ab3816d7027218bd42096eea6a07
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-05-06 11:02:09 +0200

    remove extra .pokecoin case

--- Sources/RealDeviceMap/Modell/Pokestop.swift
+++ Sources/RealDeviceMap/Modell/Pokestop.swift
@@ -368,8 +368,6 @@ class Pokestop: JSONConvertibleObject, WebHookEvent, Hashable {
             case .quest: break
             case .unset: break
             case .UNRECOGNIZED: break
-            case .pokecoin:
-                break
             }
 
             reward["info"] = infoData
