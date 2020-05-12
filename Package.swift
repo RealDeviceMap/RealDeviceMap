1b70d5f5b4c63aa9c19f2cbc59ba2b6384707d18
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-05-12 02:35:40 +0200

    update photos to 0.175.0

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.5.0")),
         .package(url: "https://github.com/123FLO321/turf-swift.git", .upToNextMinor(from: "0.3.1")),
         .package(url: "https://github.com/123FLO321/S2Geometry.git", .upToNextMinor(from: "0.3.1")),
-        .package(url: "https://github.com/123FLO321/POGOProtos-Swift.git", .upToNextMinor(from: "1.22.0"))
+        .package(url: "https://github.com/123FLO321/POGOProtos-Swift.git", .upToNextMinor(from: "1.23.0"))
     ],
     targets: [
         .target(
