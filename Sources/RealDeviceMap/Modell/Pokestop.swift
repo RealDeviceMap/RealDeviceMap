d5874ec56771af2adda318edd9dc3727d7425f51
Fabio1988 <fabio.schett@gmail.com>
2021-03-13 21:09:44 +0100

    show Pokestop when Quest is active and normal pokestops are deactivated from pokestop filter
    changed from AND to OR connection

--- Sources/RealDeviceMap/Modell/Pokestop.swift
+++ Sources/RealDeviceMap/Modell/Pokestop.swift
@@ -689,7 +689,12 @@ class Pokestop: JSONConvertibleObject, WebHookEvent, Hashable {
                                  "UNIX_TIMESTAMP())"
             let hasNoInvasionSQL = "(incident_expire_timestamp IS NULL OR incident_expire_timestamp < UNIX_TIMESTAMP())"
 
-            excludePokestopSQL = "AND ("
+            excludePokestopSQL = ""
+            if showQuests {
+                excludePokestopSQL += "OR ("
+            } else {
+                excludePokestopSQL += "AND ("
+            }
             if excludeNormal && excludeInvasion {
                 excludePokestopSQL += "(\(hasLureSQL) AND \(hasNoInvasionSQL))"
             } else if excludeNormal && !excludeInvasion {
