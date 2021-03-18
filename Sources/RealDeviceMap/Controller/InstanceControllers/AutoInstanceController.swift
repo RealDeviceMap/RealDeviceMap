e8f05ea1ed8b8d57ea88c27570a2b4ca47e32369
Petap0w <cks85@msn.com>
2021-03-18 22:49:39 +0100

    fix again

--- Sources/RealDeviceMap/Controller/InstanceControllers/AutoInstanceController.swift
+++ Sources/RealDeviceMap/Controller/InstanceControllers/AutoInstanceController.swift
@@ -45,8 +45,7 @@ class AutoInstanceController: InstanceControllerProto {
     private let accountsLock = Threading.Lock()
     private var accounts = [String: String]()
     public var delayLogout: Int
-    private let ignoreRwForQuest =
-        ProcessInfo.processInfo.environment["IGNORE_RW_FOR_QUEST"] == "true" 
+    private let ignoreRwForQuest = ProcessInfo.processInfo.environment["IGNORE_RW_FOR_QUEST"] == "true"
 
     init(name: String, multiPolygon: MultiPolygon, type: AutoType, timezoneOffset: Int,
          minLevel: UInt8, maxLevel: UInt8, spinLimit: Int, delayLogout: Int,
