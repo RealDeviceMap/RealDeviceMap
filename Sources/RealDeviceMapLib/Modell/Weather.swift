b9cbc0dc59bc832c7283bd3a8b07a6330b779838
bradders <>
2021-09-04 23:46:31 +0100

    Swiftlint fixes

--- Sources/RealDeviceMapLib/Modell/Weather.swift
+++ Sources/RealDeviceMapLib/Modell/Weather.swift
@@ -426,7 +426,7 @@ public class Weather: JSONConvertibleObject, WebHookEvent {
 
     public static func shouldUpdate(old: Weather, new: Weather) -> Bool {
         var hasChanged: Bool
-            
+
         hasChanged = (
             new.id != old.id ||
             new.level != old.level ||
@@ -440,29 +440,29 @@ public class Weather: JSONConvertibleObject, WebHookEvent {
             new.sELevel != old.sELevel ||
             new.severity != old.severity ||
             new.warnWeather != old.warnWeather)
-        
+
         if hasChanged == true {
             return true
         }
 
         let dLatitude = fabs(new.latitude - old.latitude)
         let dLongitude = fabs(new.longitude - old.longitude)
-        
+
         hasChanged = (dLatitude >= 0.000001 || dLongitude >= 0.000001)
-        
+
         if hasChanged == true {
             return true
         }
 
         let oldUpdated = old.updated ?? 0
         let newUpdated = UInt32(Date().timeIntervalSince1970)
-        
+
         hasChanged = ((newUpdated - (newUpdated % 3600)) > (oldUpdated - (oldUpdated % 3600)))
-        
+
         if hasChanged == true {
             return true
         }
-        
+
         return false
     }
 
