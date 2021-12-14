da61095a4796172769206626d201a65c8f6ebfc1
Fabio1988 <fabio.schett@gmail.com>
2021-12-14 10:57:14 +0100

    fix

--- resources/migrations/76.sql
+++ resources/migrations/76.sql
@@ -1,9 +1,9 @@
 ALTER TABLE pokestop
     ADD COLUMN `location_points` smallint(6) unsigned DEFAULT NULL AFTER `ar_scan_eligible`,
     ADD COLUMN `power_up_end_ms` int(11) unsigned DEFAULT NULL AFTER `ar_scan_eligible`,
-    ADD COLUMN `partner_id` smallint unsigned DEFAULT NULL AFTER `sponsor_id`;
+    ADD COLUMN `partner_id` varchar(35) DEFAULT NULL AFTER `sponsor_id`;
 ALTER TABLE gym
     ADD COLUMN `location_points` smallint(6) unsigned DEFAULT NULL AFTER `ar_scan_eligible`,
     ADD COLUMN `power_up_end_ms` int(11) unsigned DEFAULT NULL AFTER `ar_scan_eligible`,
-    ADD COLUMN `partner_id` smallint unsigned DEFAULT NULL AFTER `sponsor_id`;
+    ADD COLUMN `partner_id` varchar(35) DEFAULT NULL AFTER `sponsor_id`;
 
