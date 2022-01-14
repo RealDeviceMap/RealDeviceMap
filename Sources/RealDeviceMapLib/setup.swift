293c218190e90a5b67ce635c863dfc9d85d03bd1
Fabio1988 <fabio.schett@gmail.com>
2022-01-14 10:23:12 +0100

    fix error in setup if noGenerateImages is enabled

--- Sources/RealDeviceMapLib/setup.swift
+++ Sources/RealDeviceMapLib/setup.swift
@@ -191,30 +191,37 @@ public func setupRealDeviceMap() {
     Log.info(message: "[MAIN] Starting Webhook Controller")
     WebHookController.global.start()
 
+    // disable image generation, no Frontend is used
+    let noGenerateImages =
+        ProcessInfo.processInfo.environment["NO_GENERATE_IMAGES"] != nil
     // Load Forms
-    Log.info(message: "[MAIN] Loading Available Forms")
-    var availableForms = [String]()
-    do {
-        try Dir("\(Dir.projectroot)/resources/webroot/static/img/pokemon").forEachEntry { (file) in
-            let split = file.replacingOccurrences(of: ".png", with: "").components(separatedBy: "-")
-            if split.count == 2, let pokemonID = Int(split[0]), let formID = Int(split[1]) {
-                availableForms.append("\(pokemonID)-\(formID)")
-            } else if split.count == 3, let pokemonID = Int(split[0]),
-                      let formID = Int(split[1]), let evoId = Int(split[2]) {
-                availableForms.append("\(pokemonID)-\(formID)-\(evoId)")
+    if !noGenerateImages {
+        Log.info(message: "[MAIN] Loading Available Forms")
+        var availableForms = [String]()
+        do {
+            try Dir("\(Dir.projectroot)/resources/webroot/static/img/pokemon").forEachEntry { (file) in
+                let split = file.replacingOccurrences(of: ".png", with: "").components(separatedBy: "-")
+                if split.count == 2, let pokemonID = Int(split[0]), let formID = Int(split[1]) {
+                    availableForms.append("\(pokemonID)-\(formID)")
+                } else if split.count == 3, let pokemonID = Int(split[0]),
+                          let formID = Int(split[1]), let evoId = Int(split[2]) {
+                    availableForms.append("\(pokemonID)-\(formID)-\(evoId)")
+                }
             }
+            WebRequestHandler.availableFormsJson = try availableForms.jsonEncodedString()
+        } catch {
+            Log.error(
+                message: "Failed to load forms. Frontend will only display default forms. " +
+                    "Error: \(error.localizedDescription)"
+            )
         }
-        WebRequestHandler.availableFormsJson = try availableForms.jsonEncodedString()
-    } catch {
-        Log.error(
-            message: "Failed to load forms. Frontend will only display default forms. " +
-                     "Error: \(error.localizedDescription)"
-        )
+    } else {
+        Log.info(message: "[MAIN] Image generation disabled - skipping loading Forms")
     }
 
     Log.info(message: "[MAIN] Loading Available Items")
     var availableItems = [-6, -5, -4, -3, -2, -1]
-    for itemId in Item.allAvilable {
+    for itemId in Item.allAvailable {
         availableItems.append(itemId.rawValue)
     }
     WebRequestHandler.availableItemJson = try! availableItems.jsonEncodedString()
@@ -270,9 +277,7 @@ public func setupRealDeviceMap() {
     Log.info(message: "[MAIN] Starting Discord Controller")
     try! DiscordController.global.setup()
 
-    // Create Raid images
-    let noGenerateImages =
-      ProcessInfo.processInfo.environment["NO_GENERATE_IMAGES"] != nil
+    // Create images
     if !noGenerateImages {
       Log.info(message: "[MAIN] Starting Images Generator")
       ImageGenerator.generate()
