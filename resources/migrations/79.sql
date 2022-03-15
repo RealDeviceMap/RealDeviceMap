7b96bca533f681af7aba2647931e4d6aa42123ba
len <10072920+lenisko@users.noreply.github.com>
2022-03-15 01:30:04 +0100

    add ix for last_seen, might be used on fronts

--- resources/migrations/79.sql
+++ resources/migrations/79.sql
@@ -1,6 +1,3 @@
-ALTER TABLE
-  spawnpoint
-ADD
-  `last_seen` INT(11) UNSIGNED NOT NULL DEFAULT '0'
-AFTER
-  `updated`;
+ALTER TABLE spawnpoint
+  ADD `last_seen` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `updated`,
+  ADD INDEX `ix_last_seen` (`last_seen`);
