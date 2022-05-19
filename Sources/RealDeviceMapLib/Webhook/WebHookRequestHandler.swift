17a26b55e3ed31569e387dd94ce60db0c4051cd6
Fabio1988 <35898099+Fabio1988@users.noreply.github.com>
2022-05-19 22:44:51 +0200

    Fix lint

--- Sources/RealDeviceMapLib/Webhook/WebHookRequestHandler.swift
+++ Sources/RealDeviceMapLib/Webhook/WebHookRequestHandler.swift
@@ -732,8 +732,8 @@ public class WebHookRequestHandler {
                         try? pokemon!.save(mysql: mysql, updateIV: true)
                     } else {
                         if encounter.status != EncounterOutProto.Status.encounterSuccess {
-                            Log.debug(message: 
-                                "[WebHookRequestHandler] [\(uuid ?? "?")] Encounter no EncounterSuccess." +
+                            Log.debug(
+                                message: "[WebHookRequestHandler] [\(uuid ?? "?")] Encounter no EncounterSuccess." +
                                 " encounter: \(encounter)")
                             continue
                         }
