5fb841ca4db088717c53d107d2499897745bf9c6
Fabio1988 <35898099+Fabio1988@users.noreply.github.com>
2022-12-29 15:45:58 +0100

    adapt change column to modify command
    * change column is used for renaming column
    * we don't rename only resize

--- resources/migrations/94.sql
+++ resources/migrations/94.sql
@@ -1,7 +1,7 @@
 SET FOREIGN_KEY_CHECKS=0;
 ALTER TABLE `instance`
-	CHANGE COLUMN `name` `name` VARCHAR(255) NOT NULL FIRST;
+	MODIFY COLUMN `name` VARCHAR(255) NOT NULL FIRST;
 ALTER TABLE `assignment`
-	CHANGE COLUMN `instance_name` `instance_name` VARCHAR(255) NOT NULL AFTER `device_uuid`,
-	CHANGE COLUMN `source_instance_name` `source_instance_name` VARCHAR(255) NULL DEFAULT NULL AFTER `device_group_name`;
+	MODIFY COLUMN `instance_name` VARCHAR(255) NOT NULL AFTER `device_uuid`,
+	MODIFY COLUMN `source_instance_name` VARCHAR(255) NULL DEFAULT NULL AFTER `device_group_name`;
 SET FOREIGN_KEY_CHECKS=1;
