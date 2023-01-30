692b951889a20c3c6a81dff1b12c6738fa950471
Fabio1988 <fabio.schett@gmail.com>
2023-01-30 15:30:40 +0100

    change migration

--- resources/migrations/95.sql
+++ resources/migrations/95.sql
@@ -18,16 +18,3 @@ ALTER TABLE assignment
     ADD CONSTRAINT `assignment_fk_source_instance_name` FOREIGN KEY (`source_instance_name`) REFERENCES `instance` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE device
     ADD CONSTRAINT `fk_instance_name` FOREIGN KEY (`instance_name`) REFERENCES `instance` (`name`) ON DELETE SET NULL ON UPDATE CASCADE;
-
-# add new column and migrate data
-ALTER TABLE instance
-    ADD COLUMN area mediumtext GENERATED ALWAYS AS (json_extract(data, "$.area")) STORED,
-    ADD COLUMN area_new mediumtext;
-UPDATE instance
-SET area_new = area;
-ALTER TABLE instance
-    DROP COLUMN area,
-    RENAME COLUMN area_new TO area,
-    MODIFY data text not null;
-UPDATE instance
-SET data = JSON_REMOVE(data, "$.area");
