134764ed14eb666a82bd841867f2eb3c610ceeba
Fabio <fabio.schett@gmail.com>
2023-02-01 18:24:07 +0100

    fix pokemonId to int

--- Sources/RealDeviceMapLib/Modell/Stats.swift
+++ Sources/RealDeviceMapLib/Modell/Stats.swift
@@ -121,17 +121,17 @@ class Stats {
         if Stats.pokemonCountStats && old == nil || old!.cp != new.cp {
             // pokemon is new or cp has changed (eg encountered, or re-encountered)
             pokemonStatsLock.lock()
-
+            let pokemonId = new.pokemonId.toInt()
             if old == nil || old!.pokemonId != new.pokemonId { // pokemon is new or type has changed
-                pokemonCount.count[new.pokemonId] += 1
+                pokemonCount.count[pokemonId] += 1
             }
             if new.cp != nil {
-                pokemonCount.ivCount[new.pokemonId] += 1
+                pokemonCount.ivCount[pokemonId] += 1
                 if let shiny = new.shiny, shiny == true {
-                    pokemonCount.shiny[new.pokemonId] += 1
+                    pokemonCount.shiny[pokemonId] += 1
                 }
                 if let atk = new.atkIv, atk == 15, let def = new.defIv, def == 15, let sta = new.staIv, sta == 15 {
-                    pokemonCount.hundos[new.pokemonId] += 1
+                    pokemonCount.hundos[pokemonId] += 1
                 }
             }
             pokemonStatsLock.unlock()
