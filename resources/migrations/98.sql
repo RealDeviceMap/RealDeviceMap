947242d28f6fa2386f98457eeedad7c800d9310b
Fabio1988 <fabio.schett@gmail.com>
2023-03-04 23:35:04 +0100

    reset accounts set to unknown

--- resources/migrations/98.sql
+++ resources/migrations/98.sql
@@ -1,3 +1,7 @@
 ALTER TABLE account
     ADD COLUMN `disabled` tinyint(1) unsigned DEFAULT 0 AFTER `last_used_timestamp`,
     ADD COLUMN `last_disabled` int unsigned DEFAULT NULL AFTER `disabled`;
+
+UPDATE account
+SET failed = NULL, failed_timestamp = NULL
+WHERE failed = 'unknown';
