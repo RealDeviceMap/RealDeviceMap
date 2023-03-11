4ae1c93ed4e4b894ef0910199e188962ba24ccc1
Beavis <chris.e.ewing@gmail.com>
2023-03-11 10:54:59 -0700

    add synchronous URLSession.swift

---
+++ Sources/RealDeviceMapLib/Misc/URLSession.swift
@@ -0,0 +1,22 @@
+extension URLSession {
+    func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let dataTask = self.dataTask(with: url) {
+            data = $0
+            response = $1
+            error = $2
+
+            semaphore.signal()
+        }
+        dataTask.resume()
+
+        _ = semaphore.wait(timeout: .distantFuture)
+
+        return (data, response, error)
+    }
+}
