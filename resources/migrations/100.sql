9beba32658d3204f3d0c642067d8256092ed2974
Beavis <chris.e.ewing@gmail.com>
2023-04-12 06:14:39 -0600

    fix index error after field rename

--- resources/migrations/100.sql
+++ resources/migrations/100.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `spawnpoint`
 	ADD COLUMN `first_seen_timestamp` INT(11) UNSIGNED NULL DEFAULT UNIX_TIMESTAMP() AFTER `despawn_sec`,
 	ADD COLUMN `spawn_info` INT(11) UNSIGNED NULL DEFAULT 0 AFTER `created`,
-	ADD INDEX `created` (`created`),
+	ADD INDEX `first_seen_timestamp` (`first_seen_timestamp`),
 	ADD INDEX `spawn_info` (`spawn_info`);
 	
 UPDATE spawnpoint SET created=null;
