2b07d34d41a335e6f10c39b8a2e00f789255a13e
Beavis <chris.e.ewing@gmail.com>
2023-04-17 11:37:02 -0600

    apparently i need to see git next tuesday

--- resources/migrations/100.sql
+++ resources/migrations/100.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `spawnpoint`
 	ADD COLUMN `first_seen_timestamp` INT(11) UNSIGNED NULL DEFAULT UNIX_TIMESTAMP() AFTER `despawn_sec`,
-	ADD COLUMN `spawn_info` INT(11) UNSIGNED NULL DEFAULT 0 AFTER `created`,
+	ADD COLUMN `spawn_info` INT(11) UNSIGNED NULL DEFAULT 0 AFTER `first_seen_timestamp`,
 	ADD INDEX `first_seen_timestamp` (`first_seen_timestamp`),
 	ADD INDEX `spawn_info` (`spawn_info`);
 	
