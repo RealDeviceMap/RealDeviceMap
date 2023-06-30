552e3e45ee2aae1ccddcdfb79e326a48c71d7954
plinytheelder <jwjohnson310@gmail.com>
2023-06-29 18:22:32 -0700

    stop abusing your worst accounts
    title says it all

--- Sources/RealDeviceMapLib/Modell/Account.swift
+++ Sources/RealDeviceMapLib/Modell/Account.swift
@@ -609,7 +609,7 @@ public class Account: WebHookEvent {
                 \(spinSQL)
                 \(cooldownSQL)
                 \(lockoutSQL)
-            ORDER BY level DESC, last_used_timestamp DESC
+            ORDER BY level ASC, last_used_timestamp ASC
             LIMIT 1
         """
 
