97ea98abb58beb2d22ad9c25e1e1e89e495eba9f
dripdropper <cart_wheel@hotmail.com>
2024-04-11 00:37:18 -0400

    Add raid levels 11, 13, and 15 for shadow raids

--- Sources/RealDeviceMapLib/API/ApiRequestHandler.swift
+++ Sources/RealDeviceMapLib/API/ApiRequestHandler.swift
@@ -717,7 +717,7 @@ public class ApiRequestHandler {
                 ])
 
             // Level
-            for i in [1, 3, 4, 5, 6, 7, 8, 9, 10] {
+            for i in [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15] {
 
                 let raidLevel = Localizer.global.get(value: "filter_raid_level_\(i)")
 
